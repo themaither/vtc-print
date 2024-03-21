ea82c6fb0615333c63612914e7626a4f3de2d57e
TheMaither <themaither@gmail.com>
2024-03-21 11:36:23 +0200

    Added Print function

---
+++ vtc/Print.h
@@ -0,0 +1,33 @@
+#pragma once
+#include <iostream>
+#include "ToString.h"
+
+namespace vtc {
+    template <bool Newline = true>
+    void Print() 
+    {
+        if constexpr (Newline) {
+            std::cerr << "\n";
+        }
+    }
+    
+    template <bool Newline = true, ConvertibleToString T, ConvertibleToString ...TPack>
+    void Print(T value, TPack... pack) 
+    {
+        std::cerr << ToString(value);
+        Print<false, TPack...>(pack...);
+        if constexpr (Newline) {
+            std::cerr << "\n";
+        }
+    }
+
+    template <bool Newline = true, ConvertibleToString T>
+    void Print(T value) 
+    {
+        std::cerr << ToString(value);
+        if constexpr (Newline) {
+            std::cerr << "\n";
+        }
+    }
+
+}
