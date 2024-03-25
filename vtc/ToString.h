c69ce0fff51b599c86d12e71f28f5049f2ccd1bc
TheMaither <themaither@gmail.com>
2024-03-25 10:07:14 +0200

    Removed bad code

--- vtc/ToString.h
+++ vtc/ToString.h
@@ -1,28 +1,13 @@
 #pragma once
 
 #include <format>
-#include <sstream>
 #include <string>
 #include <utility>
 
+
+#include "parts/ToString_Concepts.h"
+    
 namespace vtc {
-
-template<typename T>
-std::string ToString(const T& value) {
-    return std::string(std::format("<{}>", typeid(T).name()));
-}
-
-template <typename T>
-concept IOConvertible = requires(T value, std::stringstream strs)
-{
-    strs << value;
-};
-
-template <typename T>
-concept ConvertibleToString = requires(T value)
-{
-    ToString(value);
-};
 
 template<typename T>
 requires IOConvertible<T>
@@ -33,38 +18,17 @@ std::string ToString(const T& value)
     return strs.str();
 }
 
+// template<typename T>
+// std::string ToString(const T& value) {
+//     return std::string(std::format("<{}>", typeid(T).name()));
+// }
+
 template <typename T, typename U>
 std::string ToString(const std::pair<T, U>& pair) 
 {
     return std::format("({}, {})", vtc::ToString(pair.first), vtc::ToString(pair.second));
 }
 
-template<typename T>
-requires requires(const T& value)
-{
-    { value.cbegin() };
-    { value.cend() };
-    { value.cbegin()++ };
-    { value.cend()++ };
-    { ToString(*value.cbegin()) };
-    { ToString(*value.cend()) };
-}
-std::string ToString(const T& value)
-{
-    std::stringstream result;
-    result << "[";
-    auto iter = value.cbegin();
-    while (iter != value.cend()) {
-        result << ToString(*iter);
-        auto iter_next = iter;
-        iter_next++;
-        if (iter_next != value.cend()) {
-            result << ", ";
-        }
-        iter++;
-    }
-    result << "]";
-    return  result.str();
-}
-
 } // namespace vtc
+
+#include "parts/ToString_Containers.h"
